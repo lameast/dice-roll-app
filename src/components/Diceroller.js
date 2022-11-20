22fc3dbb966e1fc895725bf1b34afaf8d4b9fdd9
Alan Wu <alanjialongwu@gmail.com>
2022-11-20 21:31:04 +1100

    Updated Diceroller component and added TotalRoll component

--- src/components/Diceroller.js
+++ src/components/Diceroller.js
@@ -5,12 +5,26 @@ import getRandomInt from '../utils';
 const Diceroller = () => {
     const [dice, setDice] = useState([6]);
     const [diceRolls, setDiceRolls] = useState([1]);
+    const [total, setTotal] = useState(1);
     
     const rollDice = (dice) => {
         return dice.map((die) => {
-            return getRandomInt(die)
+            return getRandomInt(die);
         });
     };
+
+    const onRollClick = (e) => {
+        setDiceRolls(rollDice(dice));
+        setTotal(diceRolls.reduce((a,b) => a + b, 0));
+    };
+
+    const onAddClick = (e) => {
+        const newDice = e.target.value;
+        setDice(dice => [...dice, newDice]);
+        const newDiceRoll = getRandomInt(newDice);
+        setDiceRolls(diceRolls => [...diceRolls, newDiceRoll]);
+        setTotal(total + newDiceRoll);
+    }
 
 };
 
---
+++ src/components/TotalRoll.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TotalRoll = ({total}) => {
+    return (
+        <div id="total">Total: {total}</div>
+    );
+};
+
+export default TotalRoll;
